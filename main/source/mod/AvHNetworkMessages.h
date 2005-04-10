e59df130fa22f8a2bf9ea92bb8b37eee86a7dcc1
tankefugl <tankefugl@67975925-1194-0748-b3d5-c16f83f1a3a1>
2005-04-10 21:22:03 +0000

    Mantis 0000971: o Implemented teammate orders/query icons
    Mantis 0000992:
    o Implemented teammate order popups
    
    git-svn-id: https://unknownworlds.svn.cloudforge.com/ns1@52 67975925-1194-0748-b3d5-c16f83f1a3a1

--- main/source/mod/AvHNetworkMessages.h
+++ main/source/mod/AvHNetworkMessages.h
@@ -106,6 +106,7 @@
 	void NetMsg_SetupMap_Extents( entvars_t* const pev, const string& name, const float* const min_extents, const float* const max_extents, const bool draw_background );
 	void NetMsg_SetupMap_Location( entvars_t* const pev, const string& name, const float* const min_extents, const float* const max_extents );
 	void NetMsg_UpdateEntityHierarchy( entvars_t* const pev, const MapEntityMap& NewItems, const EntityListType& OldItems );
+	void NetMsg_IssueOrder(entvars_t* const pev, const int ordertype, const int ordersource, const int ordertarget);
 
 	//BROADCAST MESSAGE TRANSMISSION
 	void NetMsg_DeathMsg( const int killer_index, const int victim_index, string& weapon_name );
@@ -195,6 +196,7 @@
 	void NetMsg_SetupMap( void* const buffer, const int size, bool& is_location, string& name, float* min_extents, float* max_extents, bool& draw_background );
 	void NetMsg_UpdateCountdown( void* const buffer, const int size, int& countdown );
 	void NetMsg_UpdateEntityHierarchy( void* const buffer, const int size, MapEntityMap& NewItems, EntityListType& OldItems );
+	void NetMsg_IssueOrder( void* const buffer, const int size, int& ordertype, int& ordersource, int& ordertarget );
 
 #endif //AVH_SERVER
 

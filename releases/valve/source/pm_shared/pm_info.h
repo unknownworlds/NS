f44f16d59a160e0739f1e1023672872ae7559fc3
puzl <puzl@67975925-1194-0748-b3d5-c16f83f1a3a1>
2005-06-08 18:52:38 +0000

    made a copy
    git-svn-id: https://unknownworlds.svn.cloudforge.com/ns1@168 67975925-1194-0748-b3d5-c16f83f1a3a1

---
+++ releases/valve/source/pm_shared/pm_info.h
@@ -0,0 +1,22 @@
+/***
+*
+*	Copyright (c) 1999, 2000, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+// Physics info string definition
+#if !defined( PM_INFOH )
+#define PM_INFOH
+#pragma once
+
+#define MAX_PHYSINFO_STRING 256
+
+#endif // PM_INFOH

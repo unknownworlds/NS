d6fe44237952ad1b28a8ddc0808f9bed5a8a33a2
tankefugl <tankefugl@67975925-1194-0748-b3d5-c16f83f1a3a1>
2005-07-14 18:17:23 +0000

    git-svn-id: https://unknownworlds.svn.cloudforge.com/ns1@281 67975925-1194-0748-b3d5-c16f83f1a3a1

--- releases/3.1/builddiff.bat
+++ releases/3.1/builddiff.bat
@@ -1,2 +1,3 @@
 @echo off (subdirs, prompt for password <-s>, date)
-c:\"Program Files"\Winzip\wzzip.exe -P -r -ex -t100105 NS-diff.zip @assets-common.txt @assets-maps.txt
+c:\"Program Files"\Winzip\wzzip.exe -P -r -ex -t080705 NS-diff.zip @assets-common.txt @assets-maps.txt
+pause
